bfdedaecfc94785423406b2255b12f1d311df168
Richard Garza <rgactr@gmail.com>
2021-04-11 20:29:32 -0700

    Javascript mastery init commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// Factory Function
+
+function createCircle(radius) {
+  const circle = {
+    radius,
+    draw: function () {
+      console.log('draw');
+    },
+  };
+}
+
+const circle = createCircle(1);
+
+// Constructor Function
+
+function Circle(radius) {
+  (this.radius = radius),
+    (this.draw = function () {
+      console.log('draw');
+    });
+}
+
+const another = new Circle(1);
+
+// I just realized JavaScript has a delete button, fml.
+delete circle;
+
+console.log(circle);
